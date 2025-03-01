662edbdf6c68ca78ab93eb853a690de1d35a79b2
MaartenGregoorPXL <maarten.gregoor@student.pxl.be>
2025-03-01 12:47:07 +0100

    input 1 en folder structure

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,10 +1,18 @@
 export default function HomePage() {
     return (
         <div className="min-h-screen flex items-center justify-center bg-[#0d1b2a] text-white">
-          <button className="px-6 py-3 bg-[#FF8C00] text-white font-bold rounded-lg text-lg shadow-md hover:bg-[#FF4B28] transition cursor-pointer">
-            Login
-          </button>
+            <div>
+                <div>
+                    <p className="px-6 py-y bg-[#FF8C00] rounded-lg">Username</p>
+                    <input className="px-6 py-3 bg-white text-gray-700 border-2 border-gray-300 rounded-lg w-full" type="text" placeholder="Enter your username" />
+                </div>
+
+                <button className="px-6 py-3 bg-[#FF8C00] w-full text-white font-bold rounded-lg text-lg shadow-md hover:bg-[#FF4B28] transition cursor-pointer">
+                Login
+                </button>
+            </div>
+            
         </div>
     );
-  }
+}
   
