796cca5a88c0593f0c0c68aaf7aa0072f80a2630
MaartenGregoorPXL <maarten.gregoor@student.pxl.be>
2025-03-01 13:03:19 +0100

    Login Page styling done

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,18 +1,38 @@
 export default function HomePage() {
     return (
         <div className="min-h-screen flex items-center justify-center bg-[#0d1b2a] text-white">
-            <div>
-                <div>
-                    <p className="px-6 py-y bg-[#FF8C00] rounded-lg">Username</p>
-                    <input className="px-6 py-3 bg-white text-gray-700 border-2 border-gray-300 rounded-lg w-full" type="text" placeholder="Enter your username" />
+            <div className="w-80">
+                {/* Username Input */}
+                <div className="relative w-full mb-12">
+                    <p className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#FF8C00] text-white px-31 py-2 rounded-lg text-center shadow-md">
+                        Username
+                    </p>
+                    <input
+                        className="w-full px-6 py-8 bg-gray-200 text-gray-700 border-2 border-gray-300 rounded-lg focus:outline-none"
+                        type="text"
+                        placeholder="Enter your username"
+                    />
                 </div>
 
-                <button className="px-6 py-3 bg-[#FF8C00] w-full text-white font-bold rounded-lg text-lg shadow-md hover:bg-[#FF4B28] transition cursor-pointer">
-                Login
-                </button>
+                {/* Password Input */}
+                <div className="relative w-full mb-6">
+                    <p className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#FF8C00] text-white px-31.5 py-2 rounded-lg text-center shadow-md">
+                        Password
+                    </p>
+                    <input
+                        className="w-full px-6 py-8 bg-gray-200 text-gray-700 border-2 border-gray-300 rounded-lg focus:outline-none"
+                        type="password"
+                        placeholder="Enter your password"
+                    />
+                </div>
+
+                {/* Login Button */}
+                <div className="w-full">
+                    <button className="px-6 py-4 bg-[#FF8C00] w-full text-white font-bold rounded-lg text-lg shadow-md hover:bg-[#FF4B28] transition cursor-pointer">
+                        Login
+                    </button>
+                </div>
             </div>
-            
         </div>
     );
 }
-  
